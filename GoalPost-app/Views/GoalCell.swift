3fc05b035f18a7bdd742edebf79a8e33b4b77407
Ryan Flores <ryanflores@ryans-mbp.attlocal.net>
2018-04-20 10:35:22 -0700

    first commit

---
+++ GoalPost-app/Views/GoalCell.swift
@@ -0,0 +1,24 @@
+//
+//  GoalCell.swift
+//  GoalPost-app
+//
+//  Created by Ryan Flores on 4/11/18.
+//  Copyright © 2018 Ryan Flores. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
