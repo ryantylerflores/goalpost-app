5cd74ee4c6ebd16629fad89ff53ddf8e2ef0e45e
Ryan Flores <ryanflores@ryans-mbp.attlocal.net>
2018-04-20 16:02:56 -0700

    GoalPost app Complete

--- GoalPost-app/Views/GoalCell.swift
+++ GoalPost-app/Views/GoalCell.swift
@@ -14,11 +14,18 @@ class GoalCell: UITableViewCell {
     @IBOutlet weak var goalDescriptionLbl: UILabel!
     @IBOutlet weak var goalTypeLbl: UILabel!
     @IBOutlet weak var goalProgressLbl: UILabel!
-
+    @IBOutlet weak var completionView: UIView!
+    
     func configureCell(goal: Goal) {
         self.goalDescriptionLbl.text = goal.goalDescription
         self.goalTypeLbl.text = goal.goalType //rawValue because GoalType is explicitly valued at String
         self.goalProgressLbl.text = String(goal.goalProgress)
+        
+        if goal.goalProgress == goal.goalCompletionValue {
+            self.completionView.isHidden = false
+        } else {
+            self.completionView.isHidden = true
+        }
     }
 
 
